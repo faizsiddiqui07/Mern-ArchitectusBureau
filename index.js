5dd4c63a64b7a658fc60a77076b591b7bd549a5d
Mohd Faiz <mohdfaiz.1842@gmail.com>
2024-09-10 15:05:41 +0530

    af'

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 8000;
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: ["http://localhost:5173", "https://front-architectus.vercel.app/"],
   credentials: true,
 }));
 app.use(express.json());
