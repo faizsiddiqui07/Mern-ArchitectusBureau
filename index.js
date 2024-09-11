b196d1e79e46b77096d881f8d53d0ec004e85c03
Mohd Faiz <mohdfaiz.1842@gmail.com>
2024-09-11 10:07:00 +0530

    ad

--- index.js
+++ index.js
@@ -12,11 +12,27 @@
   const PORT = process.env.PORT || 8000;
 
   // Middleware
+  const allowedOrigins = [
+    'http://localhost:5174',
+    'https://mern-architectus-bureau.vercel.app',
+    'https://front-architectus.vercel.app'
+  ];
+  
   app.use(cors({
-    origin: "http://localhost:5174, https://mern-architectus-bureau.vercel.app",
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
     methods: 'GET,POST,PUT,DELETE',
   }));
+  
   app.use(express.json());
   app.use(cookieParser());
   app.use(bodyParser.json());
