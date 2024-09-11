30138641eef166200f20eab3c27164ee40d3669c
Mohd Faiz <mohdfaiz.1842@gmail.com>
2024-09-11 11:20:40 +0530

    ad

--- index.js
+++ index.js
@@ -1,55 +1,39 @@
-  const express = require('express');
-  const cors = require('cors');
-  const dotenv = require('dotenv');
-  const connectDB = require('./config/db');
-  const cookieParser = require('cookie-parser');
-  const bodyParser = require('body-parser');
-  const router = require('./routes');
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const router = require('./routes');
 
-  dotenv.config();
+dotenv.config();
 
-  const app = express();
-  const PORT = process.env.PORT || 8000;
+const app = express();
+const PORT = process.env.PORT || 8000;
 
-  // Middleware
-  const allowedOrigins = [
-    'http://localhost:5174',
-    'https://front-architectus.vercel.app',
-    'https://your-production-site.com' // Replace with your actual production URL
-  ];
-  
-  app.use(cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true); // Allow non-browser requests (e.g., Postman)
-      
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      } else {
-        return callback(new Error('Not allowed by CORS'));
-      }
-    },
-    credentials: true,
-    methods: 'GET,POST,PUT,DELETE',
-  }));
+// Middleware
+app.use(cors({
+  origin: ["http://localhost:5173", "http://localhost:5174", "https://front-architectus.vercel.app"],
+  credentials: true,
+  methods: 'GET,POST,PUT,DELETE',
+}));
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use("/files", express.static("files"));
 
-  
-  app.use(express.json());
-  app.use(cookieParser());
-  app.use(bodyParser.json());
-  app.use("/files", express.static("files"));
-
-  // Routes
-  app.use('/', router);
+// Routes
+app.use('/', router);
 
 
-  // Connect to MongoDB
-  connectDB()
-    .then(() => {
-      app.listen(PORT, () => {
-        console.log('Connected to database');
-        console.log(`Server is running on port ${PORT}`);
-      });
-    })
-    .catch((err) => console.error('Error connecting to MongoDB:', err.message));
+// Connect to MongoDB
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log('Connected to database');
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.error('Error connecting to MongoDB:', err.message));
 
-  module.exports = app;
+module.exports = app;
